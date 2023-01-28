d7d8b8b84951e8ba55c772b3c768e0dc614fc3be
MylesXP <mylesparkinson12@gmail.com>
2023-01-28 01:50:44 -0800

    Added functionality to hamburger when clicked
    When hamburger is clicked, the dashboard men appears from the side and overlaps content without disturbing its placement.

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
 let hmenu = document.getElementById('header_menu');
 let hmenuHamburger = document.getElementById('header_menu__hamburger');
-let toggler = document.getElementById('toggler')
-let rtext = document.getElementsByClassName('f-right-text')[0]
+let fDash = document.getElementById('form_dashboard')
 
 hmenu.addEventListener('click',() => {
         if(hmenuHamburger.classList.contains('fa-bars')){
             hmenuHamburger.classList.remove('fa-bars')
             hmenuHamburger.classList.add('fa-x')
+            fDash.classList.add('form-dashboard--display')
         } else if (hmenuHamburger.classList.contains('fa-x')){
             hmenuHamburger.classList.remove('fa-x')
             hmenuHamburger.classList.add('fa-bars')
+            fDash.classList.remove('form-dashboard--display')
         }
-    
 });
+
+// fDash.addEventListener('click', () => {
+//     if()
+// })
 
 
